1d2402481035ace538d4ea4c64ce907cbd81d802
Łukasz <lukasz.ciaputa@gmail.com>
2018-02-28 15:01:22 +0100

    klocek lata w prawo i lewo

--- Assets/Scripts/SpaceShipController.cs
+++ Assets/Scripts/SpaceShipController.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class SpaceShipController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public GameObject vehicle;
+    float turn;
+    public int rotationSpeed=1;
+    Transform rotus
+
+	void Start ()
+    {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+	void Update ()
+    {
+
+        turn = Input.GetAxis("Horizontal") * rotationSpeed;
+        transform.Translate(turn, 0, 0);
+        if(turn<0) vehicle.transform.Rotate(0, 0, 10);
+        else if (turn>0) vehicle.transform.Rotate(0, 0, 10);
+        else vehicle.transform.Rotate(0, 0, 0);
+        transform.rotation = Quaternion.Lerp(vehicle.transform.rotation, rotus.rotation , Time.time);
+
+    }
 }
