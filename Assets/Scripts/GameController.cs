8b36af5fd6a7f8e669a7c01570395c05cc4e80f9
Łukasz <lukasz.ciaputa@gmail.com>
2018-03-14 15:00:26 +0100

    halo halo?

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject BackGround;
+    public int speed = 10;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        BackGround.transform.Rotate(Vector3.up * Time.deltaTime * speed);
+	}
+}
