18b325f8c45376cba2a63193e5a4ec03946a47bc
ThewySquare <lukasz.ciaputa@gmail.com>
2018-03-30 21:39:27 +0200

    wysuwa sie menu upgradów

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,6 +8,12 @@ public class GameController : MonoBehaviour {
     public GameObject startPoint;
     public int speed = 10;
 
+	public int travelDistance = 1;
+	public GameObject[] Shop = new GameObject[5];
+	bool UpGoing=true;
+	Vector3 startPos;
+	Vector3 endPos;
+
 	// Use this for initialization
 	void Start ()
     {
@@ -27,4 +33,20 @@ public class GameController : MonoBehaviour {
         plate.transform.rotation = Quaternion.Euler(90, 0, 0);
         Destroy(plate, 6.0f);
     }
+
+	public void ShopManager()
+	{
+		for (int i = 0; i < 5; i++) 
+		{
+			startPos = Shop [i].transform.position;
+			if (UpGoing) endPos = Shop [i].transform.position + transform.up * travelDistance;
+			else endPos = Shop [i].transform.position - transform.up * travelDistance;
+			endPos.z=0;
+			Shop [i].transform.position = Vector3.Lerp (startPos, endPos, Time.deltaTime / 10);
+		}
+		if (UpGoing)
+			UpGoing = false;
+		else
+			UpGoing = true;
+	}
 }
